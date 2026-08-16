79e4482d221fdeafd60e67781103c3e72813a207
STEPHEN ONI <stephen.oni.67@gmail.com>
2026-08-16 17:00:41 +0000

    updated

--- main.tf
+++ main.tf
@@ -1,22 +1,24 @@
-terraform {
-  required_version = ">= 1.0"
+# 1. Network Module
+module "network" {
+  source = "./modules/network"
 
-  cloud {
-    organization = "stephenoni67"
-
-    workspaces {
-      name = "stephenoni67"
-    }
-  }
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
+  vpc_cidr           = var.vpc_cidr
+  public_subnet_cidr = var.public_subnet_cidr
+  availability_zone  = var.availability_zone
 }
 
-provider "aws" {
-  region = var.aws_region
+# 2. ECR Module
+module "ecr" {
+  source = "./modules/ecr"
+}
+
+# 3. Compute Module
+module "compute" {
+  source = "./modules/compute"
+
+  vpc_id           = module.network.vpc_id
+  public_subnet_id = module.network.public_subnet_id
+  ami_id           = var.ami_id
+  instance_type    = var.instance_type
+  key_name         = var.key_name
 }
--- variables.tf
+++ variables.tf
@@ -4,4 +4,36 @@ variable "aws_region" {
   default     = "us-east-1"
 }
 
-# Add any other variable blocks here...
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+  description = "CIDR block for the public subnet"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "availability_zone" {
+  description = "AWS Availability Zone"
+  type        = string
+  default     = "us-east-1a"
+}
+
+variable "ami_id" {
+  description = "AMI ID for the EC2 instance"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "key_name" {
+  description = "Name of the SSH key pair"
+  type        = string
+}
